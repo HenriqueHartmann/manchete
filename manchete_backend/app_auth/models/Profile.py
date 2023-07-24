4443239c58c98a66d62c6186f313f1252addd9fc
HenriqueHartmann <alemaohartmann@gmail.com>
2023-07-24 15:46:28 -0300

    Add app_auth

---
+++ manchete_backend/app_auth/models/Profile.py
@@ -0,0 +1,23 @@
+import uuid
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Profile(models.Model):
+    id = models.UUIDField(
+         primary_key = True,
+         default = uuid.uuid4,
+         editable = False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user", db_column="id_user")
+    name = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return f'{self.name} | {self.user.email}'
+
+
+    class Meta:
+        db_table = "profile"
