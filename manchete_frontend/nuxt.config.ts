ed169bfdce5d4f351e6983e5161aac507cfa6a48
HenriqueHartmann <alemaohartmann@gmail.com>
2023-07-27 01:24:57 -0300

    Add font Poppins

--- manchete_frontend/nuxt.config.ts
+++ manchete_frontend/nuxt.config.ts
@@ -1,11 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css',],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-})
+  modules: [
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Poppins: true,
+        },
+      },
+    ],
+  ],
+});
